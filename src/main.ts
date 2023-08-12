33b0f7dbfcf4ee0a0cb2bba12aeb5bd2f7914e31
Alexander Kislov <strannik11.92@gmail.com>
2023-08-12 17:58:44 +0300

    Implement interpolation flow

--- src/main.ts
+++ src/main.ts
@@ -1,47 +1,80 @@
 import './style.css';
 
-let resultUrl: string;
+const TEMPLATE = `{
+  "cc": "US",
+  "city": "Pasadena",
+  "country": "United States",
+  "formatted_address": "8120 Jumpers Hole Road, Pasadena, MD, United States",
+  "formatted_city": "Pasadena, MD, United States",
+  "postal_code": "21122",
+  "state": "MD",
+  "street_address": "8120 Jumpers Hole Road"
+}`;
 
-const inputResult = document.querySelector<HTMLInputElement>('#result')!;
-const form = document.querySelector<HTMLFormElement>('#url-concat')!;
-const textareaResult = document.querySelector<HTMLFormElement>('#result')!;
-const btnCopy = document.querySelector<HTMLFormElement>('#copy')!;
-const btnOpen = document.querySelector<HTMLFormElement>('#open')!;
-const btnClear = document.querySelectorAll<HTMLFormElement>('.c_url__btn--clear')!;
+const intForm = document.querySelector<HTMLFormElement>('#int-form')!;
+const inputTemplate = document.querySelector<HTMLInputElement>('#int-template')!;
+const intStr = document.querySelector<HTMLFormElement>('#int-string')!;
+const intResult = document.querySelector<HTMLFormElement>('#int-result')!;
+const clearResultBtns = document.querySelectorAll<HTMLFormElement>('.c_int__btn--clear')!;
+const copyBtn = document.querySelector<HTMLFormElement>('.c_int__btn--copy')!;
+
+copyBtn.disabled = true;
 
 const formHandler = (event: SubmitEvent) => {
   event.preventDefault();
-  const formData = new FormData(event.target as HTMLFormElement);
-  const data = Object.fromEntries(formData.entries());
-  const result = `http://${data.localhost}:${data.port}/?providerId=${data.providerId}&token=${data.token}`;
-  resultUrl = result;
-  inputResult.value = resultUrl;
+  intResult.value = interpolate(inputTemplate.value);
+  checkForEmptyState();
+};
+
+const interpolate = (template: string) => {
+  const templateObj = JSON.parse(template);
+  const parsedInput = parseString(intStr.value);
+  return `{
+  "cc": "${templateObj.cc}",
+  "city": "${parsedInput.city}",
+  "country": "${parsedInput.country}",
+  "formatted_address": "${parsedInput.formatted_address}",
+  "formatted_city": "${parsedInput.formatted_city}",
+  "postal_code": "${parsedInput.postal_code}",
+  "state": "${parsedInput.state}",
+  "street_address": "${parsedInput.street_address}"
+}`;
+};
+
+const parseString = (str: string) => {
+  const inputArr = str.split(',').map(item => item.trim());
+  const inputSubArr = inputArr[2].split(' ');
+
+  const street = inputArr[0];
+  const city = inputArr[1];
+  const state = inputSubArr[0];
+  const postal = inputSubArr[1];
+  const country = inputArr[3];
+
+  return {
+    city: city,
+    country: country,
+    formatted_address: `${street}, ${city}, ${state}, ${country}`,
+    formatted_city: `${city}, ${state}, ${country}`,
+    postal_code: postal,
+    state: state,
+    street_address: street,
+  };
 };
 
 const copyContent = async () => {
-  if (!resultUrl) return;
-
   try {
-    await navigator.clipboard.writeText(resultUrl);
+    await navigator.clipboard.writeText(intResult.value);
+    copyBtn.innerText = 'Done';
     console.log('Content copied to clipboard');
-    console.info(`🌻 ${resultUrl}`);
-    btnCopy.innerText = 'Done';
+    console.info(`${intResult.value}`);
   } catch (err) {
     console.error('Failed to copy: ', err);
-    btnCopy.innerText = 'Some error';
   } finally {
     setTimeout(() => {
-      btnCopy.innerText = 'Copy to clipboard';
+      copyBtn.innerText = 'Copy';
     }, 1000);
-    clearResult();
   }
-};
-
-const openInNewTab = () => {
-  if (!resultUrl) return;
-
-  window.open(resultUrl, '_blank');
-  clearResult();
 };
 
 const clearInput = (event: MouseEvent) => {
@@ -51,14 +84,20 @@ const clearInput = (event: MouseEvent) => {
 
   if (input instanceof HTMLInputElement || input instanceof HTMLTextAreaElement) {
     input.value = '';
+    checkForEmptyState();
   }
 };
 
-const clearResult = () => {
-  textareaResult.value = '';
+function checkForEmptyState() {
+  if (intResult.value.trim()) {
+    copyBtn.disabled = false;
+  } else {
+    copyBtn.disabled = true;
+  }
 }
 
-form.addEventListener('submit', formHandler);
-btnCopy.addEventListener('click', copyContent);
-btnOpen.addEventListener('click', openInNewTab);
-btnClear.forEach(btn => btn.addEventListener('click', clearInput));
+inputTemplate.value = TEMPLATE;
+intForm.addEventListener('submit', formHandler);
+copyBtn.addEventListener('click', copyContent);
+clearResultBtns.forEach(btn => btn.addEventListener('click', clearInput));
+intResult.addEventListener('input', checkForEmptyState);
